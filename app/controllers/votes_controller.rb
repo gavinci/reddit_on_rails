cbe65944c8a9284c6d30a368aed063205fc5800f
Gavin Chan <gavinci@gmail.com>
2013-07-08 17:26:28 -0700

    Vote validation complete

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,14 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @vote = current_user.votes.create(params[:vote])
+    @vote = Vote.where(user_id: current_user.id, link_id: params[:vote][:link_id]).first
+    if @vote
+      @vote.up = params[:vote][:up]
+      @vote.save
+    else
+      @vote = current_user.votes.create(params[:vote])
+    end
+    
     redirect_to :back
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,8 @@ class Vote < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :user_id, :link_id, :up
   
+  validates :user_id, :uniqueness => { :scope => :link_id }
+  
   belongs_to :user
   belongs_to :link
 end
